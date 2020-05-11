eca5eb514797447023586302850d02f38428030b
Vishakh Rao <vishakh.171it150@nitk.edu.in>
2020-05-11 22:20:52 +0530

    Forms part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,10 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 
 
@@ -54,7 +58,11 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule 
 
   ],
    entryComponents: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,11 +6,18 @@ import {MatDialog, MatDialogRef} from '@angular/material/dialog';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = {username: '', password: '', remember: false};
+
+  constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log('User: ', this.user);
+    this.dialogRef.close();
+  }
 }
