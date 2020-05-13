fa0cc69b48cb48d8b19e4c9a8948856402ab825b
Vishakh Rao <vishakh.171it150@nitk.edu.in>
2020-05-13 23:43:10 +0530

    RxJs Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -25,5 +25,8 @@ getDishes(): Observable<Dish[]> {
   getFeaturedDish(): Observable<Dish> {
     return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
   }
+    getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
+  }
 }
   
